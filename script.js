a83ba2c62b29f9638d2cfc61a4eee3637946b72b
sSsamirRr-mine <sssamirrr4@gmail.com>
2024-12-25 14:47:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let gap = prompt("so'z yozing")
+
+let h1 = document.querySelector('h1')
+
+h1.innerHTML = gap
+
+h1.style.color = 'Blue'
+
+h1.style.border = '2px, solid, blue'
+
+h1.style.width = '200px'
+
+h1.style.height = '200px'
+
+h1.style.display = 'flex'
+
+h1.style.alignItems = 'center'
+
+h1.style.justifyContent = 'center'
